a922b0d3ca5e2c6841ba3e30b8098c607407b605
eliot2013 <151815278+eliot2013@users.noreply.github.com>
2023-11-30 01:14:15 +0800

    Update main.go

--- main.go
+++ main.go
@@ -19,5 +19,13 @@ func main() {
 			"message": "display msg",
 		})
 	})
+
+
+	r.GET("/show", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "show msg",
+		})
+	})
+	
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
